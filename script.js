502ee9a4e16f36622b3b72b5b30704645f5a57f8
DominikKnapczyk <dominik.kna.01@gmail.com>
2023-01-24 19:39:21 +0100

    final commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function generaPrezzo() {
   var distanza = document.getElementById("distanza").value;
   var eta = document.getElementById("eta").value;
+  var nome = document.getElementById("nome").value;
   
   if (eta == 'maggiorenne') {
    var prezzo = distanza * 0.21;
@@ -10,5 +11,11 @@ function generaPrezzo() {
     var prezzo = (distanza * 0.21) * (0.6);
   }
 
-  document.getElementById("prezzo").innerHTML = "Il prezzo del biglietto &egrave; " + prezzo.toFixed(2) + " &euro;";
+  document.getElementById("prezzo").innerHTML = prezzo.toFixed(2) + " &euro;";
+  document.getElementById("nome-biglietto").innerHTML = nome;
+  document.getElementById('biglietto-generato').style.display = "block";
+}
+
+function annullaBiglietto() {
+  location.reload();
 }
